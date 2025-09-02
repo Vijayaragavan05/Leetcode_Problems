67f1446090bbc5a2b7c2d825250da7bab609079f
Vijayaragavan <vijayaragavan4818@gmail.com>
2025-09-02 19:29:20 +0530

    Time: 0 ms (100%), Space: 41.4 MB (41.25%) - LeetHub

--- 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,11 +1,18 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for(int n:nums){
-            if(n>0){
-                set.add(n);
-            }
+        // Set<Integer> set = new HashSet<>();
+        // for(int n:nums){
+        //     if(n>0){
+        //         set.add(n);
+        //     }
+        // }
+        // return set.size();
+
+        int ans[] = new int[101];
+        int c = 0;
+        for(int i:nums){
+            if(ans[i]++==0) c++;
         }
-        return set.size();
+        return c - (ans[0]==0?0:1);
     }
 }
