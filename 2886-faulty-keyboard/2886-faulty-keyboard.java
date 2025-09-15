af3b620e3a93b6227c4013130cf07b10b99e7ef1
Vijayaragavan <vijayaragavan4818@gmail.com>
2025-09-15 21:17:22 +0530

    Time: 18 ms (6.31%), Space: 45.5 MB (11.37%) - LeetHub

---
+++ 2886-faulty-keyboard/2886-faulty-keyboard.java
@@ -0,0 +1,47 @@
+// class Solution {
+//     public String finalString(String s) {
+//         String ans = "";
+//         for(int i=0;i<s.length();i++){
+//             char ch =s.charAt(i);
+//             if(ch=='i'){
+//                 ans+=rev(ans);
+//             }
+//             else{
+//                 ans+=ch;
+//             }
+//         }
+//         return ans;
+//     }
+//     public String rev(String ch){
+//         String s ="";
+//         int n = ch.length();
+//         for(int i=n-1;i>=0;i--){
+//             s+=ch.charAt(i);
+//         }
+//         return s;
+//     }
+// }
+
+class Solution {
+    public String finalString(String s) {
+        String ans = "";
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == 'i') {
+                ans = rev(ans);
+            } else {
+                ans += ch;
+            }
+        }
+        return ans;
+    }
+
+    public String rev(String ch) {
+        String s = "";
+        int n = ch.length();
+        for (int i = n - 1; i >= 0; i--) {
+            s += ch.charAt(i);
+        }
+        return s;
+    }
+}
