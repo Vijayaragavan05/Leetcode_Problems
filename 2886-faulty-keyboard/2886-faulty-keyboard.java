cdebbb4157ab130c589ba5f19a42c820ebe06be1
Vijayaragavan <vijayaragavan4818@gmail.com>
2025-09-15 21:17:30 +0530

    Time: 3 ms (96.18%), Space: 45.3 MB (17.43%) - LeetHub

--- 2886-faulty-keyboard/2886-faulty-keyboard.java
+++ 2886-faulty-keyboard/2886-faulty-keyboard.java
@@ -1,47 +1,35 @@
-// class Solution {
-//     public String finalString(String s) {
-//         String ans = "";
-//         for(int i=0;i<s.length();i++){
-//             char ch =s.charAt(i);
-//             if(ch=='i'){
-//                 ans+=rev(ans);
-//             }
-//             else{
-//                 ans+=ch;
-//             }
-//         }
-//         return ans;
-//     }
-//     public String rev(String ch){
-//         String s ="";
-//         int n = ch.length();
-//         for(int i=n-1;i>=0;i--){
-//             s+=ch.charAt(i);
-//         }
-//         return s;
-//     }
-// }
-
 class Solution {
     public String finalString(String s) {
-        String ans = "";
-        for (int i = 0; i < s.length(); i++) {
+    //     String ans = "";
+    //     for (int i = 0; i < s.length(); i++) {
+    //         char ch = s.charAt(i);
+    //         if (ch == 'i') {
+    //             ans = rev(ans);
+    //         } else {
+    //             ans += ch;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    // public String rev(String ch) {
+    //     String s = "";
+    //     int n = ch.length();
+    //     for (int i = n - 1; i >= 0; i--) {
+    //         s += ch.charAt(i);
+    //     }
+    //     return s;
+
+        StringBuilder str = new StringBuilder();
+        for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            if (ch == 'i') {
-                ans = rev(ans);
-            } else {
-                ans += ch;
+            if(ch!='i'){
+                str.append(ch);
+            }
+            else{
+                str.reverse();
             }
         }
-        return ans;
-    }
-
-    public String rev(String ch) {
-        String s = "";
-        int n = ch.length();
-        for (int i = n - 1; i >= 0; i--) {
-            s += ch.charAt(i);
-        }
-        return s;
-    }
+        return str.toString();
+    } 
 }
