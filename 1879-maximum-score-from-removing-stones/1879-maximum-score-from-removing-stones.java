76e3288ab9b1f6e453e20d83de1955ecd3970781
Vijayaragavan <vijayaragavan4818@gmail.com>
2025-09-27 14:17:11 +0530

    Time: 0 ms (100%), Space: 40.6 MB (72.43%) - LeetHub

--- 1879-maximum-score-from-removing-stones/1879-maximum-score-from-removing-stones.java
+++ 1879-maximum-score-from-removing-stones/1879-maximum-score-from-removing-stones.java
@@ -1,16 +1,9 @@
 class Solution {
     public int maximumScore(int a, int b, int c) {
-        // int sum=a+b+c;
-        // int max = Math.max(a,Math.max(b,c));
-        // if(max>sum-max) return sum-max;
-        // else return sum/2;
-        int arr[] = {a,b,c};
-        Arrays.sort(arr);
-        int sum1=arr[0]+arr[1];
-        int sum2=arr[2];
-        int res=0;
-        if(sum1>=sum2)  res=sum2+(sum1-sum2)/2;
-        else res=sum1;
-        return res;
+        int sum=a+b+c;
+        int max = Math.max(a,Math.max(b,c));
+        if(max>sum-max) return sum-max;
+        else return sum/2;
+        
     }
 }
