956e22a511251ed6f86cd6b9897efa54b4878ba5
Vijayaragavan <vijayaragavan4818@gmail.com>
2025-10-07 18:53:59 +0530

    Time: 8 ms (6.8%), Space: 43.7 MB (5.38%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int getLucky(String s, int k) {
+        String alp = "abcdefghijklmnopqrstuvwxyz";
+        String str ="";
+        for(char ch : s.toCharArray()){
+            str+=alp.indexOf(ch)+1;
+        }
+        return value(str,k);
+    }
+    public int value(String s1,int k){
+        int sum=0;
+        while(k-- >0){
+            sum=0;
+            for(char ch : s1.toCharArray()){
+                sum+=ch-'0';
+            }
+            s1 = String.valueOf(sum);
+        }
+        return sum;
+    }
+}
